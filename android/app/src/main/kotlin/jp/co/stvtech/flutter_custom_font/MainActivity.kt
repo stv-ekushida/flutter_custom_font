f98292648efe280175c98d30672b5be356e2f3c7
e.kushida <e.kushida@stv-tech.co.jp>
2021-05-13 12:39:58 +0900

    カスタムフォントの設定

---
+++ android/app/src/main/kotlin/jp/co/stvtech/flutter_custom_font/MainActivity.kt
@@ -0,0 +1,6 @@
+package jp.co.stvtech.flutter_custom_font
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/AppTheme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static const fontFamily = 'Roboto';
+
+  static ThemeData get() {
+    return ThemeData(
+      fontFamily: fontFamily,
+    );
+  }
+}
